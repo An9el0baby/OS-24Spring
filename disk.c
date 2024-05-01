bbaf180d22b0d181a0bdf3423d86e5eeb8afa6ad
Angelo Wu <an9el0wu@gmail.com>
2024-05-01 14:22:42 -0400

    fix

--- disk.c
+++ disk.c
@@ -101,74 +101,134 @@ struct RCB handle_request_arrival_look(struct RCB request_queue[QUEUEMAX], int *
     }
 }
 
-struct RCB handle_request_completion_look(struct RCB request_queue[QUEUEMAX], int *queue_cnt, int current_cylinder, int scan_direction)
+// struct RCB handle_request_completion_look(struct RCB request_queue[QUEUEMAX], int *queue_cnt, int current_cylinder, int scan_direction)
+// {
+//     if (*queue_cnt == 0)
+//     {
+//         return NULLRCB;
+//     }
+
+//     int closest = 0;
+//     int closest_distance = -1;
+
+//     for (int i = 0; i < *queue_cnt; i++)
+//     {
+//         int distance = request_queue[i].cylinder - current_cylinder;
+//         if (distance == 0)
+//         {
+//             int earliest = 0;
+//             for (int j = 1; j < *queue_cnt; j++)
+//             {
+//                 if (request_queue[j].arrival_timestamp < request_queue[earliest].arrival_timestamp)
+//                 {
+//                     earliest = j;
+//                 }
+//             }
+//             struct RCB earliest_request = request_queue[earliest];
+//             for (int j = earliest; j < *queue_cnt - 1; j++)
+//             {
+//                 request_queue[j] = request_queue[j + 1];
+//             }
+//             *queue_cnt -= 1;
+//             return earliest_request;
+//         }
+//         if (scan_direction == 1)
+//         {
+//             if (distance >= 0 && (closest_distance == -1 || distance < closest_distance))
+//             {
+//                 closest = i;
+//                 closest_distance = distance;
+//             }
+//         }
+//         else
+//         {
+//             if (distance <= 0 && (closest_distance == -1 || -distance < closest_distance))
+//             {
+//                 closest = i;
+//                 closest_distance = -distance;
+//             }
+//         }
+//     }
+
+//     if (closest == -1)
+//     {
+//         closest_distance = -1;
+//         for (int i = 0; i < *queue_cnt; i++)
+//         {
+//             int distance = request_queue[i].cylinder - current_cylinder;
+//             if (closest_distance == -1 || abs(distance) < closest_distance)
+//             {
+//                 closest = i;
+//                 closest_distance = abs(distance);
+//             }
+//         }
+//     }
+
+//     struct RCB closest_request = request_queue[closest];
+//     for (int i = closest; i < *queue_cnt - 1; i++)
+//     {
+//         request_queue[i] = request_queue[i + 1];
+//     }
+//     *queue_cnt -= 1;
+//     return closest_request;
+// }
+struct RCB handle_request_completion_look(struct RCB request_queue[QUEUEMAX], int *queue_cnt, int current_cylinder, int *scan_direction)
 {
     if (*queue_cnt == 0)
     {
         return NULLRCB;
     }
 
-    int closest = 0;
-    int closest_distance = -1;
+    int closest = -1;
+    int closest_distance = INT_MAX;
 
     for (int i = 0; i < *queue_cnt; i++)
     {
         int distance = request_queue[i].cylinder - current_cylinder;
-        if (distance == 0)
+
+        if ((*scan_direction == 1 && distance >= 0) || (*scan_direction == -1 && distance <= 0))
         {
-            int earliest = 0;
-            for (int j = 1; j < *queue_cnt; j++)
-            {
-                if (request_queue[j].arrival_timestamp < request_queue[earliest].arrival_timestamp)
-                {
-                    earliest = j;
-                }
-            }
-            struct RCB earliest_request = request_queue[earliest];
-            for (int j = earliest; j < *queue_cnt - 1; j++)
-            {
-                request_queue[j] = request_queue[j + 1];
-            }
-            *queue_cnt -= 1;
-            return earliest_request;
-        }
-        if (scan_direction == 1)
-        {
-            if (distance >= 0 && (closest_distance == -1 || distance < closest_distance))
+            if (abs(distance) < closest_distance)
             {
                 closest = i;
-                closest_distance = distance;
+                closest_distance = abs(distance);
             }
-        }
-        else
-        {
-            if (distance <= 0 && (closest_distance == -1 || -distance < closest_distance))
+            else if (abs(distance) == closest_distance && request_queue[i].arrival_timestamp < request_queue[closest].arrival_timestamp)
             {
                 closest = i;
-                closest_distance = -distance;
             }
         }
     }
 
     if (closest == -1)
     {
-        closest_distance = -1;
+        *scan_direction = -*scan_direction;
+        closest_distance = INT_MAX;
+
         for (int i = 0; i < *queue_cnt; i++)
         {
             int distance = request_queue[i].cylinder - current_cylinder;
-            if (closest_distance == -1 || abs(distance) < closest_distance)
+
+            if (abs(distance) < closest_distance)
             {
                 closest = i;
                 closest_distance = abs(distance);
+            }
+            else if (abs(distance) == closest_distance && request_queue[i].arrival_timestamp < request_queue[closest].arrival_timestamp)
+            {
+                closest = i;
             }
         }
     }
 
     struct RCB closest_request = request_queue[closest];
+
     for (int i = closest; i < *queue_cnt - 1; i++)
     {
         request_queue[i] = request_queue[i + 1];
     }
+
     *queue_cnt -= 1;
+
     return closest_request;
 }
