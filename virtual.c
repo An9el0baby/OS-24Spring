1010792b0ec9ae0b04fee135cadbaafd0ec0a14d
Angelo Wu <an9el0wu@gmail.com>
2024-05-01 11:49:36 -0400

    lfu done

--- virtual.c
+++ virtual.c
@@ -141,7 +141,63 @@ int count_page_faults_lru(struct PTE page_table[TABLEMAX], int table_cnt, int re
 }
 int process_page_access_lfu(struct PTE page_table[TABLEMAX], int *table_cnt, int page_number, int frame_pool[POOLMAX], int *frame_cnt, int current_timestamp)
 {
+    if (page_table[page_number].is_valid == 1)
+    {
+        page_table[page_number].last_access_timestamp = current_timestamp;
+        page_table[page_number].reference_count++;
+        return page_table[page_number].frame_number;
+    }
+    if (*frame_cnt > 0)
+    {
+        *frame_cnt -= 1;
+        int frame_number = frame_pool[*frame_cnt];
+        page_table[page_number].is_valid = 1;
+        page_table[page_number].frame_number = frame_number;
+        page_table[page_number].arrival_timestamp = current_timestamp;
+        page_table[page_number].last_access_timestamp = current_timestamp;
+        page_table[page_number].reference_count = 1;
+        return frame_number;
+    }
+    int min_lfu_timestamp = INT_MAX;
+    int min_index = -1;
+    for (int i = 0; i < *table_cnt; i++)
+    {
+        if (page_table[i].is_valid && page_table[i].reference_count < min_lfu_timestamp)
+        {
+            min_lfu_timestamp = page_table[i].reference_count;
+            min_index = i;
+        }
+    }
+    if (min_index != -1)
+    {
+        int frame_number = page_table[min_index].frame_number;
+        page_table[min_index].is_valid = 0;
+        page_table[min_index].frame_number = -1;
+        page_table[min_index].arrival_timestamp = -1;
+        page_table[min_index].last_access_timestamp = -1;
+        page_table[min_index].reference_count = -1;
+
+        page_table[page_number].is_valid = 1;
+        page_table[page_number].frame_number = frame_number;
+        page_table[page_number].arrival_timestamp = current_timestamp;
+        page_table[page_number].last_access_timestamp = current_timestamp;
+        page_table[page_number].reference_count = 1;
+        return frame_number;
+    }
 }
+
 int count_page_faults_lfu(struct PTE page_table[TABLEMAX], int table_cnt, int refrence_string[REFERENCEMAX], int reference_cnt, int frame_pool[POOLMAX], int frame_cnt)
 {
+    int page_faults = 0;
+    for (int i = 0; i < reference_cnt; i++)
+    {
+        int frame_number_1 = page_table[refrence_string[i]].frame_number;
+        int current_timestamp = i + 1;
+        int frame_number = process_page_access_lfu(page_table, &table_cnt, refrence_string[i], frame_pool, &frame_cnt, current_timestamp);
+        if (frame_number_1 != frame_number)
+        {
+            page_faults++;
+        }
+    }
+    return page_faults;
 }
